5db88b56be9f71309bca50305672ecd7118d37e4
derlomo <lomo@DESKTOP-FHV7810>
2024-01-03 18:11:24 +0100

    final changes done

--- src/layouts/Home/Profile.js
+++ src/layouts/Home/Profile.js
@@ -53,6 +53,17 @@ export const Profile = ({ id, visible, sectionRef }) => {
         {visible => (
           <div className={styles.content}>
             <div className={styles.column}>
+              <ProfileText visible={visible} titleId={titleId} />
+              <Button
+                secondary
+                className={styles.button}
+                data-visible={visible}
+                icon="send"
+              >
+                Read more
+              </Button>
+            </div>
+            <div className={styles.column}>
               <div className={styles.tag} aria-hidden>
                 <Divider
                   notchWidth="64px"
