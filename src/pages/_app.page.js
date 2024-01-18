43b865e10cdf1e0ddb22dd34526085ca362c75b0
DerLomo <hardcoreminecraft5@gmail.com>
2024-01-18 15:06:55 +0100

    Update _app.page.js

--- src/pages/_app.page.js
+++ src/pages/_app.page.js
@@ -17,11 +17,7 @@ import { ScrollRestore } from '../layouts/App/ScrollRestore';
 
 export const AppContext = createContext({});
 
-const repoPrompt = `
-__  __  __
-\u005C \u005C \u005C \u005C \u005C\u2215\n \u005C \u005C\u2215\u005C \u005C\n  \u005C\u2215  \u005C\u2215
-\n\nTaking a peek huh? Check out the source code: https://github.com/HamishMW/portfolio
-`;
+const repoPrompt = ``;
 
 const App = ({ Component, pageProps }) => {
   const [storedTheme] = useLocalStorage('theme', 'dark');
